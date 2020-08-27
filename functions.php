f44268b1a87f29885d839a9088dc9eeb309e8267
松村拓海 <upoponas@matsumurayuurinnoMacBook.local>
2020-08-27 11:29:31 +0900

    サイドバー分離成功

--- functions.php
+++ functions.php
@@ -1,12 +1,44 @@
 <?php
-function create_post_type() {
-  register_post_type( "profile", // 投稿タイプ名の定義
-  array(
-    "label" =>  "プロフィール" , // 表示する投稿タイプ名
-    "public" => true,
-    "menu_position" =>5,
+function create_post_type()
+{
+  register_post_type(
+    "profile", // 投稿タイプ名の定義
+    array(
+      "label" =>  "プロフィール", // 表示する投稿タイプ名
+      "public" => true,
+      "menu_position" => 5,
     )
   );
 }
 // カスタム投稿タイプの追加
-add_action( "init", "create_post_type" );
+add_action("init", "create_post_type");
+
+//投稿カテゴリ編集項目追加
+add_action('edit_category_form_fields', 'extra_category_fields');
+function extra_category_fields($tag)
+{
+  $t_id = $tag->term_id;
+  $cat_meta = get_option("cat_$t_id");//カテゴリの情報をidによって識別して取得
+?>
+  <tr class="form-field">
+    <th><label for="extra_text">fontawesomeアイコンclass</label></th>
+    <td><input type="text" name="Cat_meta[extra_text]" id="extra_text" size="25" value="<?php if (isset($cat_meta['extra_text'])) echo esc_html($cat_meta['extra_text']) ?>" /></td>
+    <!-- issetで何かその中に入っているか判別 -->
+  </tr>
+<?php
+}
+add_action('edited_term', 'save_extra_category_fileds');
+function save_extra_category_fileds($term_id)
+{
+  if (isset($_POST['Cat_meta'])) {
+    $t_id = $term_id;
+    $cat_meta = get_option("cat_$t_id");
+    $cat_keys = array_keys($_POST['Cat_meta']);
+    foreach ($cat_keys as $key) {
+      if (isset($_POST['Cat_meta'][$key])) {
+        $cat_meta[$key] = $_POST['Cat_meta'][$key];
+      }
+    }
+    update_option("cat_$t_id", $cat_meta);
+  }
+}
---
+++ sidebar.php
@@ -0,0 +1,33 @@
+<aside>
+  <div class="author">
+    <img src="assets/images/nas.png" alt="">
+    <h4><span>うぽぽ茄子</span></h4>
+    <p>現在神戸大学に在籍しながらwebエンジニアとして活動中。サークルとバイトで消耗し、飲み会でお金を全てドブに捨てる周りの大学生をみて、このままでは折角高いお金で大学に通わせてもらってるのに、自分もあんな風になってしまうと思い独学でプログラミングを学び始めました。今後色々なことにチャレンジしていきたいなと考えております。夏は大嫌いなので一切外に出たくありません。観光とショッピングが大好きです。</p>
+    <ul class="url">
+      <li class="profile">
+        <a href="/">プロフィール<span><i class="fas fa-arrow-circle-right"></i></span></a></li>
+      <li class="order"><a href="/">Web制作依頼<span><i class="fas fa-arrow-circle-right"></i></span></a></li>
+      <li class="contact"><a href="/">お問い合わせ<span><i class="fas fa-arrow-circle-right"></i></span></a></li>
+    </ul>
+  </div>
+
+  <div class="search-form">
+    <form action="https://google.co.jp/search" method="get" target="_blank">
+      <input type="text" name="s" size="31" id="s" placeholder="">
+
+    </form>
+  </div>
+  <div class="menu">
+    <div class="title">MENU</div>
+    <nav>
+      <?php get_template_part('category'); ?>
+    </nav>
+  </div>
+  <div class="archive">
+    <div class="title">ARCHIVE</div>
+    <ul>
+      <li><a href="">2020年8月<span>3</span></a></li>
+      <li><a href="">2020年7月<span>0</span></a></li>
+    </ul>
+  </div>
+</aside>
